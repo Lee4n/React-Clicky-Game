21eabbcc2a5e0003f42f80fbbfb199c57b0944db
Lee Lawler <lawler_lee@icloud.com>
2020-05-03 14:15:48 -0500

    added navbar component

---
+++ clicky-game/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from './components/Navbar'
+import Header from './components/Header'
+
+const App = () => 
+    <div>
+        <Navbar />
+        <Header />
+    </div>
+
+export default App;
---
+++ clicky-game/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navbar = () => (
+  <nav>
+    <div class="nav-wrapper row">
+      <div class="container">
+      <a href="#" class="brand-logo">Clicky Game</a>
+      <ul class="right">
+        <li>Click an image to begin!</li>
+        <li>Score: 0 | Top Score: 0</li>
+      </ul>
+      </div>
+    </div>
+  </nav>
+);
+
+export default Navbar;
